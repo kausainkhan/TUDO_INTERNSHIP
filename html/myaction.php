2b2ef82ce80ff3c1e60b26228a649827a0128da3
kausain <kausainkhan1000@gmail.com>
2022-07-01 13:20:53 +0530

    second commit

---
+++ html/myaction.php
@@ -0,0 +1,10 @@
+<?php
+$name =$_GET('name');
+$email =$_GET('email');
+$password =$_GET('password');
+$phone =$_GET('phone');
+
+echo 'WELCOME' .name.' ' .email .' ' .password .' ' .phone;
+echo '<br/>';
+
+?>
---
+++ js/storage.js
@@ -0,0 +1,23 @@
+// console.log('hi everyone')
+// let impArray=['Tudo','Technologies','Bangalore']
+// localStorage.setItem('Name', 'Kausain')
+// localStorage.setItem('Name2', 'Ayush')
+// localStorage.setItem('Company', JSON.stringify(impArray));
+
+// let name = localStorage.getItem('Name');
+//  name = JSON.parse(localStorage.getItem('Company'));
+// console.log(name)
+
+// sessionStorage.setItem('sessionName', 'sKausain')
+// sessionStorage.setItem('sessionName2', 'sAyush')
+// sessionStorage.setItem('sessionCompany', JSON.stringify(impArray));
+
+
+
+// to clear localstorage
+// localStorage.clear();
+// claer particular key
+//localStorage.removeItem('Name)
+
+
+document.cookie='item=flower; expires=mon,04 july 2022 12:00:00 UTC';
